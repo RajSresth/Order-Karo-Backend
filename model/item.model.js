c3b29a9dc2c6c708aa2c96c62fedf55add71d963
Shresth Rajput <rajshresth2@gmail.com>
2026-05-04 10:58:04 +0530

    Item Model

---
+++ model/item.model.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    shop: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    category: {
+      type: String,
+      enum: [
+        "snacks",
+        "Main Course",
+        "Desserts",
+        "Pizza",
+        "Burgers",
+        "Sandwiches",
+        "South Indian",
+        "Chinese",
+        "Fast Food",
+        "Others",
+      ],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    foodType: {
+      type: String,
+      enum: ["veg", "non veg"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Item = mongoose.model("Item", itemSchema);
+export default Item;
